d08c081eb7b5045a60c4742e50eb9e1e1941d43b
botagar <botagar@imagimaps.com>
2024-08-07 20:04:25 +1000

    more cleanup from old world of things

--- infrastructure/platform/data.tf
+++
@@ -1,7 +0,0 @@
-data "aws_ec2_managed_prefix_list" "cloudfront" {
-  name = "com.amazonaws.global.cloudfront.origin-facing"
-}
-
-data "aws_ec2_managed_prefix_list" "r53_healthchecks" {
-  name = "com.amazonaws.${var.aws_region}.route53-healthchecks"
-}
--- infrastructure/platform/image_storage.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "imagimap_services_bff" {
-  name                 = "imagimap_services_bff"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
